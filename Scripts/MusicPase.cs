f4696d73e2693249d7214fb6c516eca62545b441
unknown <daichi19990821@gmail.com>
2022-07-02 12:17:09 +0900

    update samplescene to musicscene

--- Scripts/MusicPase.cs
+++ Scripts/MusicPase.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 /// <summary>
 /// テンポに応じてAudioSourceの再生速度を変える
@@ -11,17 +12,36 @@ public class MusicPase : MonoBehaviour
     /// </summary>
     [SerializeField] float threshold = 0.1f;
 
+    /// <summary>
+    /// テンポが1秒間に変化する量
+    /// </summary>
+    [SerializeField] float tempoChangeSpeed = 10;
+
     const float ORIGINAL_TEMPO = 0.56f;
+
+    /// <summary>
+    /// 現在の目標テンポ
+    /// </summary>
+    float currentTargetTempo = 0.0f;
+
+    /// <summary>
+    /// 現在の音楽のテンポ
+    /// </summary>
+    float currentTempo = 0.0f;
 
     private void Start() {
         holder.RegisterOnBeat(OnBeat);
     }
 
     private void OnBeat(BeatPacket packet){
-        float tempo = packet.Tempo;
-        if(Mathf.Abs(tempo - ORIGINAL_TEMPO) <= threshold){
-            tempo = ORIGINAL_TEMPO;
+        currentTargetTempo = packet.Tempo;
+        if(Mathf.Abs(currentTargetTempo - ORIGINAL_TEMPO) <= threshold){
+            currentTargetTempo = ORIGINAL_TEMPO;
         }
-        audioSource.pitch = ORIGINAL_TEMPO / tempo;
+    }
+
+    IEnumerator TempoChange(){
+        
+        yield return null;
     }
 }
