49d95a52a4901156f04417f2daa35b8ac1e50715
u975265h <u975265h@ecs.osaka-u.ac.jp>
2022-08-25 21:19:33 +0900

    finalized by Hirobe-san

--- Scripts/UIControll/JustTimingUpdater.cs
+++ Scripts/UIControll/JustTimingUpdater.cs
@@ -3,18 +3,8 @@ using UnityEngine.UI;
 /// <summary>
 /// 拍動イベントを受けてUIを更新するコンポーネント
 /// </summary>
-public class UIUpdater : MonoBehaviour
+public class JustTimingUpdater : MonoBehaviour
 {
-    /// <summary>
-    /// 拍動回数のテキスト
-    /// </summary>
-    [SerializeField] Text beatCountText;
-
-    /// <summary>
-    /// 現在のテンポのテキスト
-    /// </summary>
-    [SerializeField] Text tempoText;
-
     /// <summary>
     /// テンポが正しく刻まれた回数を示すテキスト
     /// </summary>
@@ -32,20 +22,8 @@ public class UIUpdater : MonoBehaviour
 
     private void Start()
     {
-        beatMakerHolder.RegisterOnBeat(OnBeat);
-
         var m = FindObjectOfType<MusicPase>();
         m.RegisterOnJustTiming(this.OnJustTiming);
-    }
-
-    /// <summary>
-    /// 拍動発生時に呼ばれるイベント
-    /// </summary>
-    /// <param name="packet">拍動に関する情報</param>
-    void OnBeat(BeatPacket packet)
-    {
-        beatCountText.text = $"BeatCount : {packet.BeatCount}";
-        tempoText.text = $"Tempo : {packet.Tempo}";
     }
 
     /// <summary>
